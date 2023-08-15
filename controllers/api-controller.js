4736f8a83f536283abf1b438f1cfbac0e11bff11
73rax <alexanderpaok11@gmail.com>
2023-08-15 16:25:13 +0100

    Initial folder structure and KNEX setup

---
+++ controllers/api-controller.js
@@ -0,0 +1,4 @@
+const knex = require('knex')(require('../knexfile'));
+
+
+//exports.
---
+++ server.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8000;
+const apiRoutes = require('./')
+const cors = require("cors");
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use('/api', apiRoutes)
+
+app.listen(PORT, () => {
+    console.log(`running at http://localhost:${PORT}`);
+})
