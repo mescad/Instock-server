23244e8636a148a66833a32b16d1d2cb0589911f
Riccardo <r.golin@hotmail.it>
2023-08-16 12:41:44 +0100

    all werehouses and single werehouses api endpoint completed

--- controllers/api-controller.js
+++ controllers/api-controller.js
@@ -1,4 +1,46 @@
 const knex = require('knex')(require('../knexfile'));
 
+exports.getAllWarehouse = async (req, res) => {
+  const warehouses = await knex('warehouses').select([
+    ' warehouse_name',
+    'address',
+    'city',
+    'country',
+    'contact_name',
+    'contact_position',
+    'contact_phone',
+    'contact_email'
+  ]);
+  //rico
+  res.json(warehouses);
+};
+
+exports.getOneWarehouse = async (req, res) => {
+  try {
+    const warehouse = await knex('warehouses')
+      .where({ id: req.params.id })
+      .select([
+        ' warehouse_name',
+        'address',
+        'city',
+        'country',
+        'contact_name',
+        'contact_position',
+        'contact_phone',
+        'contact_email'
+      ])
+      .first(); //rico
+    if (!warehouse) {
+      return res.status(404).json({
+        message: `Warehouse with ID: ${req.params.id} not found`
+      });
+    }
+    res.json(warehouse);
+  } catch (error) {
+    res.status(500).json({
+      message: `Unable to retrive user data with ID: ${req.params.id}`
+    }); //rico
+  }
+};
 
 //exports.
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const apiController = require("../controllers/api-controller");
+const apiController = require('../controllers/api-controller');
 
+router.route('/warehouses').get(apiController.getAllWarehouse); //rico
+
+router.route('/warehouses/:id').get(apiController.getOneWarehouse); //rico
 
 module.exports = router;
