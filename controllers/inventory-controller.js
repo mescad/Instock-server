e8df5a6f7e3b098d30ec98b8053bcfcfcfcec4d9
73rax <alexanderpaok11@gmail.com>
2023-08-16 13:45:11 +0100

    Minor changes

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -1,21 +1,28 @@
 const knex = require("knex")(require("../knexfile"));
 
 exports.getAll = async (req, res) => {
-  const allItems = await knex("inventories");
-  res.json(allItems);
+  const allItems = await knex("warehouses")
+    .join("inventories", "inventories.warehouse_id", "warehouses.id")
+    .select("inventories.id", "warehouses.warehouse_name", "inventories.item_name", "inventories.description", "inventories.category", "inventories.status", "inventories.quantity");
+
+  res.status(200).json(allItems);
 };
 
 exports.getOne = async (req, res) => {
   try {
-    const item = await knex("inventories").where({ id: req.params.id }).first();
+    const items = await knex("warehouses")
+      .join("inventories", "inventories.warehouse_id", "warehouses.id")
+      .select("inventories.id", "warehouses.warehouse_name", "inventories.item_name", "inventories.description", "inventories.category", "inventories.status", "inventories.quantity")
+      .where({ "inventories.id": req.params.id });
 
-    if (!item) {
+    if (!items) {
       return res.status(404).json({
         message: `Item with ID: ${req.params.id} not found`,
       });
     }
-    res.json(item);
+    res.json(items);
   } catch (err) {
+    console.log(err);
     res.status(500).json({
       message: `Unable to retrieve item data for item with ID: ${req.params.id}`,
     });
